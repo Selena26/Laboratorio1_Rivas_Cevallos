535c8926740916d1745d6777165b877460408433
Joan C <jfcevallos7@espe.edu.ec>
2021-02-26 12:10:42 -0500

    Agrege una clase empleado
    creo que sale en java

---
+++ Empleado.java
@@ -0,0 +1,37 @@
+package modelo.mundo;
+
+public class Empleado extends Persona{
+	
+	private String cargo;
+	private double sueldo;
+
+	public Empleado(String nombre, int cedula, String cargo, double sueldo) {
+		super(nombre, cedula);
+		this.cargo = cargo;
+		this.sueldo = sueldo;
+
+
+	}
+
+	public String getCargo() {
+		return cargo;
+	}
+
+	public double getSueldo() {
+		return sueldo;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Empleado [cargo=" + cargo + ", sueldo=" + sueldo + ", genero=" + ", nombre=" + nombre
+				+ ", cedula=" + cedula + "]";
+	}
+	
+	
+	
+	
+	
+	
+
+}
